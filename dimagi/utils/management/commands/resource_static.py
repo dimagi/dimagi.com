304cf444f83b1eb9ab452229ec0d76badfbfda31
Biyeun Buczyk <biyeun@dimagi.com>
2017-11-30 22:15:38 -0500

    add management command to version static files

---
+++ dimagi/utils/management/commands/resource_static.py
@@ -0,0 +1,76 @@
+from __future__ import absolute_import, print_function
+import hashlib
+import json
+import os
+import six
+from django.core.management.base import BaseCommand
+from django.contrib.staticfiles import finders
+from django.conf import settings
+from django.core import cache
+from dimagi.utils import gitinfo
+
+
+default_cache = cache.caches['default']
+RESOURCE_PREFIX = '#resource_%s'
+
+
+class Command(BaseCommand):
+    help = "Prints the paths of all the static files"
+
+    root_dir = settings.PROJECT_ROOT
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--clear',
+            action='store_true',
+            default=False,
+        )
+
+    def current_sha(self):
+        current_snapshot = gitinfo.get_project_snapshot(self.root_dir, submodules=False)
+        sha = current_snapshot['commits'][0]['sha']
+        print("Current commit SHA: %s" % sha)
+        return sha
+
+    def output_resources(self, resources):
+        with open(os.path.join(self.root_dir, 'resource_versions.py'), 'w') as fout:
+            fout.write("resource_versions = %s" % json.dumps(resources, indent=2))
+
+    def overwrite_resources(self, resources, sha=None):
+        if not sha:
+            sha = self.current_sha()
+        default_cache.set(RESOURCE_PREFIX % sha, resources, 86400)
+        self.output_resources(resources)
+
+    def handle(self, **options):
+        prefix = os.getcwd()
+
+        if options['clear']:
+            print("clearing resource cache")
+            default_cache.delete_pattern(RESOURCE_PREFIX % '*')
+
+        current_sha = self.current_sha()
+        existing_resources = default_cache.get(RESOURCE_PREFIX % current_sha, None)
+        if existing_resources and not isinstance(existing_resources, six.string_types):
+            print("getting resource dict from cache")
+            self.output_resources(existing_resources)
+            return
+
+        resources = {}
+        for finder in finders.get_finders():
+            for path, storage in finder.list(['.*', '*~', '* *']):
+                if not storage.location.startswith(prefix):
+                    continue
+                if not getattr(storage, 'prefix', None):
+                    url = path
+                else:
+                    url = os.path.join(storage.prefix, path)
+                filename = os.path.join(storage.location, path)
+                resources[url] = self.get_hash(filename)
+        self.overwrite_resources(resources, sha=current_sha)
+
+    @staticmethod
+    def get_hash(filename):
+        with open(filename) as f:
+            file_hash = hashlib.sha1(f.read()).hexdigest()[:7]
+        return file_hash
