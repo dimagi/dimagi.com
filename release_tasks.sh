9a88b9439d96b3950dd4133d0f00d7fb616fef13
Biyeun Buczyk <biyeun@dimagi.com>
2017-12-05 23:36:28 -0500

    support compressor and CDN in release tasks

--- release_tasks.sh
+++ release_tasks.sh
@@ -3,6 +3,8 @@
 rm -r ./staticfiles
 python manage.py compilescss
 python manage.py collectstatic --ignore=*.scss --clear --no-input
+python manage.py compress
 rm static_versions.py
 python manage.py build_static_versions
 python manage.py compilescss --delete-files
+python manage.py s3_pushstatic
