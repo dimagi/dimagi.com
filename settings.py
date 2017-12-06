9505d7119ebad681b9a90311d252162777c2a3d0
Biyeun Buczyk <biyeun@dimagi.com>
2017-12-06 02:37:12 -0500

    update compress_storage

--- settings.py
+++ settings.py
@@ -113,7 +113,7 @@ STATIC_URL = env.str('STATIC_URL', default='/static/')
 
 if AWS_ENABLED:
     STATICFILES_STORAGE = "dimagi.storage.StaticFileStorage"
-    COMPRESS_STORAGE = STATICFILES_STORAGE
+    COMPRESS_STORAGE = 'dimagi.storage.S3BotoStorage'
 
 SASS_ASSETS = static_assets_root('style')
 SASS_PROCESSOR_INCLUDE_DIRS = [
