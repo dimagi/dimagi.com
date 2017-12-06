bd5e6bf2be8463c7eb89e8f8cef5da1b5096cfa6
Biyeun Buczyk <biyeun@dimagi.com>
2017-12-05 23:33:26 -0500

    add some other useful template filters

--- dimagi/utils/templatetags/dimagidotcom.py
+++ dimagi/utils/templatetags/dimagidotcom.py
@@ -31,3 +31,28 @@ def static(url):
         static_url = "{}?version={}".format(static_url, version)
 
     return static_url
+
+
+def _escape_script_tags(unsafe_str):
+    # seriously: http://stackoverflow.com/a/1068548/8207
+    return unsafe_str.replace('</script>', '<" + "/script>')
+
+
+@register.filter
+def JSON(obj):
+    # json.dumps does not properly convert QueryDict array parameter to json
+    if isinstance(obj, QueryDict):
+        obj = dict(obj)
+    try:
+        return mark_safe(_escape_script_tags(json.dumps(obj, default=json_handler)))
+    except TypeError as e:
+        raise e
+
+
+@register.filter
+def BOOL(obj):
+    try:
+        obj = obj.to_json()
+    except AttributeError:
+        pass
+    return 'true' if obj else 'false'
