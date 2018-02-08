46b656af120cc14580a28bb881108e13c323ab8b
Biyeun Buczyk <biyeun@dimagi.com>
2018-02-08 16:13:14 -0500

    add commcare evidence base toolkit

--- dimagi/toolkits/data/__init__.py
+++ dimagi/toolkits/data/__init__.py
@@ -1,10 +1,12 @@
 from dimagi.toolkits.data import (
     maturity_model,
     total_cost_ownership,
+    commcare_evidence_base,
 )
 
 
 toolkits = (
     maturity_model,
     total_cost_ownership,
+    commcare_evidence_base,
 )
---
+++ dimagi/toolkits/data/commcare_evidence_base.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+from django.utils.translation import ugettext_lazy
+from dimagi.toolkits.models import Toolkit
+
+
+TOOLKIT = Toolkit(
+    title=ugettext_lazy(
+        "The CommCare Evidence Base"
+    ),
+    tagline=ugettext_lazy(
+        "Over 50 studies have assessed CommCare's impact, making it the most "
+        "evidence-based mobile platform for frontline workers in low-resource "
+        "settings."
+    ),
+    template="toolkits/summaries/commcare_evidence_base.html",
+    slug="commcare-evidence-base",
+    download_url="https://www.dropbox.com/s/chn7t7dsubhe9qb/CommCare-Evidence-Base-July-2016.pdf?dl=1",
+)
