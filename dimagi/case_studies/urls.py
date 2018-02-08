4c7637143b9b039c3eda30bc34073e3514359311
Biyeun Buczyk <biyeun@dimagi.com>
2018-02-08 15:51:04 -0500

    update case studies urls

--- dimagi/case_studies/urls.py
+++ dimagi/case_studies/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from dimagi.case_studies.views import view_single, download
+from dimagi.case_studies.views import view_all, view_single, download
 
 urlpatterns = [
+    url(r'^$', view_all,
+        name='case_studies'),
     url(r'^(?P<slug>[\w-]+)/$', view_single,
         name='case_study'),
     url(r'^(?P<slug>[\w-]+)/download/$', download,
