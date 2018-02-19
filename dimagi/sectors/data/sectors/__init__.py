8602929cefd65245d0feb0d1aedc1b5ffc66daf1
Marcin Kaczmarczyk <mkaczmarczyk@soldevelo.com>
2018-02-19 13:20:08 +0100

    270462 Dimagi.com Sector: Agricultural Finance

--- dimagi/sectors/data/sectors/__init__.py
+++ dimagi/sectors/data/sectors/__init__.py
@@ -1,10 +1,12 @@
 from dimagi.sectors.data.sectors import (
     child_health,
     education,
+    agricultural_finance
 )
 
 
 all_sectors = (
     child_health,
     education,
+    agricultural_finance
 )
