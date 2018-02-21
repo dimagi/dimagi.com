98f52a47473482e567ab7170ed09265ec196c1aa
Biyeun Buczyk <biyeun@dimagi.com>
2018-02-21 18:42:47 -0500

    integrate greenhouse

--- dimagi/careers/urls.py
+++ dimagi/careers/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
 
-from dimagi.careers.views import home
+from dimagi.careers.views import home, view_job
 
 
 urlpatterns = [
     url(r'^$', home,
         name='careers'),
+    url(r'^job/(?P<job_id>[\d]+)/$', view_job,
+        name='careers_job'),
 ]
