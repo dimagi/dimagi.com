3fea3afb250a0105077777c4e721cd5fd9060b49
Biyeun Buczyk <biyeun@dimagi.com>
2018-03-14 16:39:50 -0400

    fix forms in modals on ios

--- assets/js/app.js
+++ assets/js/app.js
@@ -53,5 +53,16 @@ require([
     ], function (m) {
       m.initialize();
     });
+
+    var ua = navigator.userAgent,
+    iOS = /iPad|iPhone|iPod/.test(ua),
+    iOS11 = /OS 11_0|OS 11_1|OS 11_2/.test(ua);
+
+    // ios 11 bug caret position
+    if ( iOS && iOS11 ) {
+      // Add CSS class to body
+      $("html").addClass("ios-modal-fix");
+    }
+
   });
 });
