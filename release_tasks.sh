29e038bcf87438b699034b2f55d1e8a566b3f949
Biyeun Buczyk <biyeun@dimagi.com>
2018-03-21 14:52:29 -0400

    update release tasks

--- release_tasks.sh
+++ release_tasks.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-rm -r ./staticfiles
+python manage.py build_thumbnails
 python manage.py compilescss
 python manage.py collectstatic --ignore=*.scss --clear --no-input
 python manage.py compress
-rm static_versions.py
 python manage.py build_static_versions
 python manage.py compilescss --delete-files
 python manage.py s3_pushstatic
