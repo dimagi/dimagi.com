3a819c9c5ac4d58009b47ce77f6762e3d227da6e
Biyeun Buczyk <biyeun@dimagi.com>
2018-07-05 19:40:21 -0400

    Revert "load hsforms async"
    This reverts commit 20f857e85f509c0074001495c2f0e96d3e3d6ecc.

--- assets/js/app/contactForm.js
+++ assets/js/app/contactForm.js
@@ -11,21 +11,15 @@ define([
   return {
     initialize: function () {
       var apiId = Utils.getApiId('HUBSPOT'),
-          contactFormId = $("meta[property='dimagi:contactFormId']").attr('content'),
-          scriptUrls = [
-            '//js.hsforms.net/forms/v2.js',
-          ];
+          contactFormId = $("meta[property='dimagi:contactFormId']").attr('content');
 
       if (contactFormId && apiId) {
-        $.when.apply($, _.map(scriptUrls, function(url) { return $.getScript(url); }))
-          .done(function() {
-            hbspt.forms.create({
-              portalId: apiId,
-              formId: contactFormId,
-              target: "#contact-form-content",
-              css: ""
-            });
-          });
+        hbspt.forms.create({
+          portalId: apiId,
+          formId: contactFormId,
+          target: "#contact-form-content",
+          css: ""
+        });
       }
 
     },
