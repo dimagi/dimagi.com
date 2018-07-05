20f857e85f509c0074001495c2f0e96d3e3d6ecc
Biyeun Buczyk <biyeun@dimagi.com>
2018-07-05 19:07:14 -0400

    load hsforms async

--- assets/js/app/contactForm.js
+++ assets/js/app/contactForm.js
@@ -11,15 +11,21 @@ define([
   return {
     initialize: function () {
       var apiId = Utils.getApiId('HUBSPOT'),
-          contactFormId = $("meta[property='dimagi:contactFormId']").attr('content');
+          contactFormId = $("meta[property='dimagi:contactFormId']").attr('content'),
+          scriptUrls = [
+            '//js.hsforms.net/forms/v2.js',
+          ];
 
       if (contactFormId && apiId) {
-        hbspt.forms.create({
-          portalId: apiId,
-          formId: contactFormId,
-          target: "#contact-form-content",
-          css: ""
-        });
+        $.when.apply($, _.map(scriptUrls, function(url) { return $.getScript(url); }))
+          .done(function() {
+            hbspt.forms.create({
+              portalId: apiId,
+              formId: contactFormId,
+              target: "#contact-form-content",
+              css: ""
+            });
+          });
       }
 
     },
