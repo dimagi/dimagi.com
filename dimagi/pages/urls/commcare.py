9353c74f75d55691d404351b83e6b191e3edf78d
Biyeun Buczyk <biyeun@dimagi.com>
2018-08-22 20:44:26 -0500

    add pricing pdf url enpoints

--- dimagi/pages/urls/commcare.py
+++ dimagi/pages/urls/commcare.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 
-from dimagi.pages.views.commcare import product, pricing
+from dimagi.pages.views.commcare import (
+    product,
+    pricing,
+    handle_pricing_pdf,
+)
 
 
 urlpatterns = [
@@ -8,4 +12,8 @@ urlpatterns = [
         name='commcare'),
     url(r'^pricing/$', pricing,
         name='commcare_pricing'),
+    url(r'^pricing/commcare-pricing-monthly.pdf$', handle_pricing_pdf(True),
+        name='monthly_commcare_pricing_pdf'),
+    url(r'^pricing/commcare-pricing-annual.pdf$', handle_pricing_pdf(False),
+        name='annual_commcare_pricing_pdf'),
 ]
