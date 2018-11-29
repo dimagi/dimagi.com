04adcf504f23b256f2b355d5c4ccb651385cfb22
Biyeun Buczyk <biyeun@dimagi.com>
2018-11-29 13:18:55 -0600

    add enable_amp and set_amp_page_type decorators to the mdc pillar view

--- dimagi/pages/views/pillar.py
+++ dimagi/pages/views/pillar.py
@@ -1,6 +1,11 @@
 from __future__ import absolute_import
 from django.shortcuts import render
 
+from dimagi.utils.decorators.amp_page_type import set_amp_page_type, AmpPageType
+from dimagi.utils.decorators.enable_amp import enable_amp
 
+
+@enable_amp
+@set_amp_page_type(AmpPageType.PILLAR)
 def mobile_data_collection(request):
     return render(request, 'pages/pillar/mobile_data_collection.html')
