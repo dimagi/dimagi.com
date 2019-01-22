8be9df0da37ae2f2a445b41e1b38474e24694cda
Biyeun Buczyk <biyeun@dimagi.com>
2019-01-22 09:59:48 -0600

    add initial styleguide content

--- dimagi/styleguide/views.py
+++ dimagi/styleguide/views.py
@@ -8,3 +8,21 @@ from dimagi.utils.decorators import hide_drift, no_index
 @hide_drift
 def home(request):
     return render(request, 'styleguide/home.html')
+
+
+@no_index
+@hide_drift
+def sizes(request):
+    return render(request, 'styleguide/sizes.html')
+
+
+@no_index
+@hide_drift
+def grid(request):
+    return render(request, 'styleguide/grid.html')
+
+
+@no_index
+@hide_drift
+def colors(request):
+    return render(request, 'styleguide/colors.html')
