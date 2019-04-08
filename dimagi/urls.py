585766e77529d0129fa8ccebacd7b0fa1696d3e2
hemanthReddy06 <hemanth8877@gmail.com>
2019-04-08 19:30:10 +0530

    Added updated url for data collection plan

--- dimagi/urls.py
+++ dimagi/urls.py
@@ -77,6 +77,8 @@ urlpatterns = [
         redirect.blog('mobile-data-collection-mobile-survey-design')),
     url(r'^blog/mobile-data-collection-implement-and-train/$',
         redirect.blog('mobile-data-collection-app-deploy')),
+    url(r'^blog/what-is-your-data-collection-process/$',
+        redirect.blog('data-collection-data-collection-plan')),
 
     url(r'^blog/', include(blog.blog_urls)),
     url(r'^about/', include(team.about_urls)),
