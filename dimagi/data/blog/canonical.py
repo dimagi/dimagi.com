0a2e4bffc761a17bccf2a0ecfb61096c143f34a2
Ramarao <yadlapalli9999@gmail.com>
2019-05-21 12:22:52 +0530

    updated canonical links for mdc and dc pillar page

--- dimagi/data/blog/canonical.py
+++ dimagi/data/blog/canonical.py
@@ -2,16 +2,21 @@ from __future__ import absolute_import
 
 from dimagi.pages.models.blog import CanonicalLink
 
-MDC_PILLAR_URL = "https://dimagi.com/mobile-data-collection/"
+MDC_PILLAR_URL = "https://dimagi.com/mobile-data-collection/",
+DC_PILLAR_URL =  "https://dimagi.com/data-collection/"
 
 POSTS = [
-    CanonicalLink('what-are-your-project-objectives', MDC_PILLAR_URL),
-    CanonicalLink('what-is-your-data-collection-process', MDC_PILLAR_URL),
-    CanonicalLink('mobile-data-collection-data-requirements', MDC_PILLAR_URL),
     CanonicalLink('mobile-data-collection-how-to-choose-the-right-tool', MDC_PILLAR_URL),
-    CanonicalLink('mobile-data-collection-standards', MDC_PILLAR_URL),
+    CanonicalLink('mobile-data-collection-mobile-survey-design', MDC_PILLAR_URL),
+    CanonicalLink('mobile-data-collection-app-build', MDC_PILLAR_URL),
+    CanonicalLink('mobile-data-collection-app-test', MDC_PILLAR_URL),
+    CanonicalLink('mobile-data-collection-app-deploy', MDC_PILLAR_URL),
+    CanonicalLink('mobile-data-collection-training', MDC_PILLAR_URL),
     CanonicalLink('mobile-data-collection-sustain', MDC_PILLAR_URL),
-    CanonicalLink('mobile-data-collection-introduction', MDC_PILLAR_URL),
+    CanonicalLink('data-collection-project-objectives', DC_PILLAR_URL),
+    CanonicalLink('data-collection-data-requirements', DC_PILLAR_URL),
+    CanonicalLink('data-collection-method-of-data-collection', DC_PILLAR_URL),
+    CanonicalLink('data-collection-data-collection-plan', DC_PILLAR_URL)
 ]
 
 
