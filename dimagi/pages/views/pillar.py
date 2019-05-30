bf2f285eff911f3aa88b006307f4485831cef432
Biyeun Buczyk <biyeun@dimagi.com>
2019-05-30 21:05:47 +0200

    cleanup

--- dimagi/pages/views/pillar.py
+++ dimagi/pages/views/pillar.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import
 from django.shortcuts import render
 
+from dimagi.utils.ab_tests import DEMO_WORKFLOW_V2
 from dimagi.utils.decorators.amp_page_type import set_amp_page_type, AmpPageType
+from dimagi.utils.decorators.enable_ab_test import enable_ab_test
 from dimagi.utils.decorators.enable_amp import enable_amp
 
 
@@ -10,5 +12,6 @@ from dimagi.utils.decorators.enable_amp import enable_amp
 def mobile_data_collection(request):
     return render(request, 'pages/pillar/mobile_data_collection.html')
 
+@enable_ab_test(DEMO_WORKFLOW_V2)
 def data_collection(request):
     return render(request, 'pages/pillar/data_collection.html')
