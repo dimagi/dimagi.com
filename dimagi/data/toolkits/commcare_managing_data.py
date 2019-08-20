cb545bc18e0cf0ea0f41af2586d0c2bfd3556959
hemanth <hemanth8877@gmail.com>
2019-08-20 19:32:19 +0530

    removed digitial interventions in py files

--- dimagi/data/toolkits/commcare_managing_data.py
+++ dimagi/data/toolkits/commcare_managing_data.py
@@ -58,7 +58,6 @@ TOOLKIT.add_other_toolkits([
     summary.COMMCARE_EVIDENCE_BASE,
     summary.MANAGING_DEVICES_EBOOK,
     summary.MATURITY_MODEL,
-    summary.DIGITAL_HEALTH_INTERVENTIONS,
     summary.DATA_COLLECTION,
     summary.MOBILE_DATA_COLLECTION,
     summary.TOTAL_COST_OWNERSHIP,
