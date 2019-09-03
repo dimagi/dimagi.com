11c7416b1ca8a510d124c8d507a9f10b969af830
hemanth <hemanth8877@gmail.com>
2019-09-03 14:38:51 +0530

    updated language

--- dimagi/data/case_studies/mhealth/malaria_mozambique.py
+++ dimagi/data/case_studies/mhealth/malaria_mozambique.py
@@ -5,42 +5,39 @@ from dimagi.pages.models.case_studies import CaseStudy
 
 STUDY = CaseStudy(
     title=ugettext_lazy(
-        "Improving Integrated Community Case Management in Mozambique"
+        "Malaria Consortium upSCALE: iCCM for Improving Child Health "
     ),
     summary=ugettext_lazy(
-        "The inSCALE project seeks to demonstrate that government-led "
-        "Integrated Community Case Management (iCCM) can improve "
-        "healthcare services and expand coverage in Mozambique, "
-        "a country where only 52% of the population has healthcare "
-        "coverage. The project adopted CommCare and CommConnect to "
-        "strengthen communication between community health workers "
-        "(CHWs) and health facility supervisors, and with heavy "
-        "involvement of the Ministry of Health, inSCALE has the potential "
-        "to expand into other areas of the health system, with the "
-        "goal of improved diagnosis, treatment, and monitoring of disease "
-        "throughout Mozambique."
+        "The upSCALE project uses Integrated Community Case Management "
+        "(iCCM) to improve healthcare services and expand coverage in "
+        "Mozambique, a country where only 52% of the population has "
+        "healthcare coverage. The project adopted CommCare to strengthen "
+        "communication between community health workers (CHWs) and health "
+        "facility supervisors. With Ministry of Health staff now responsible "
+        "for content design, training, roll-out, and platform hosting, upSCALE "
+        "has the potential to expand into other areas of the health system, "
+        "with the goal of improved diagnosis, treatment, and monitoring of "
+        "disease throughout Mozambique."
     ),
     partners=[
+        "Mozambique Ministry of Health",
         "Malaria Consortium",
-        "Dimagi",
+        "UNICEF",
     ],
     countries=[
         ugettext_lazy("Mozambique"),
     ],
     sectors=[
-        ugettext_lazy("child health"),
-        ugettext_lazy("pneumonia"),
-        ugettext_lazy("malaria"),
-        ugettext_lazy("iCCM"),
+        ugettext_lazy("Maternal & Child Health"),
+        ugettext_lazy("Disease Treatment"),
     ],
     features=[
-        ugettext_lazy("active data management"),
-        ugettext_lazy("decision & diagnostic support"),
-        ugettext_lazy("respiratory rate counter"),
-        ugettext_lazy("motivational messages (for CHWs & Supervisors)"),
-        ugettext_lazy("case management"),
-        ugettext_lazy("multimedia"),
-        ugettext_lazy("custom reporting"),
+        ugettext_lazy("Case Management"),
+        ugettext_lazy("Decision & Diagnostic Support"),
+        ugettext_lazy("Respiratory Rate Counter"),
+        ugettext_lazy("SMS Notifications"),
+        ugettext_lazy("Multimedia"),
+        ugettext_lazy("Custom Reporting"),
     ],
     slug="mhealth-malaria-mozambique",
     download_url="https://www.dropbox.com/s/trffquc2e3ra0gw/mhealth-malaria-mozambique.pdf?dl=1",
