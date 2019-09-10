3c10e0b40e52f37603d95cba6d0209316466154f
hemanth <hemanth8877@gmail.com>
2019-09-10 14:30:38 +0530

    Added content (Image, PDF) pending

--- dimagi/data/case_studies/mhealth/supply_chain_tanzania.py
+++ dimagi/data/case_studies/mhealth/supply_chain_tanzania.py
@@ -5,28 +5,36 @@ from dimagi.pages.models.case_studies import CaseStudy
 
 STUDY = CaseStudy(
     title=ugettext_lazy(
-        "Leveraging SMS to Inform Supply Chain in Tanzania"
+        "ILSGateway: CommCare for Improving the Supply Chain"
     ),
     summary=ugettext_lazy(
-        "As part of a larger effort to integrate and streamline "
-        "logistics management in Tanzania, the USAID | DELIVER PROJECT "
-        "and the Ministry of Health and Social Welfare (MOHSW) established "
-        "the ILSGateway in 2008 in order to expand accessibility and "
-        "visibility of logistics data to inform supply chain decision "
-        "making through mobile technology."
+        "The Tanzanian Ministry of Health and Social Welfare created "
+        "ILSGateway to monitor the supply of lifesaving medication at health "
+        "facilities throughout Tanzania. After struggling with delays in "
+        "stock tracking and requisition submissions, their new application "
+        "expands the accessibility and visibility of logistics data and "
+        "informs supply chain decision-making. The app monitors a set of "
+        "tracer commodities, family planning and anti-malarial medications, "
+        "and worker performance."
     ),
     partners=[
-        "Tanzania Ministry of Health",
-        "USAID | DELIVER Project",
-        "Dimagi",
+        "USAID",
+        "Tanzania Ministry of Health and Social Welfare",
+        "John Snow, Inc.",
     ],
     countries=[
         ugettext_lazy("Tanzania"),
     ],
     sectors=[
-        ugettext_lazy("Supply Chain Management"),
+        ugettext_lazy("Disease Treatment"),
+        ugettext_lazy("Maternal & Child Health"),
     ],
-    features=[],
+    features=[
+        ugettext_lazy("Data Collection"),
+        ugettext_lazy("Referrals"),
+        ugettext_lazy("SMS Reminders"),
+        ugettext_lazy("Custom Reports"),
+    ],
     slug="mhealth-supply-chain-tanzania",
     download_url="https://www.dropbox.com/s/suffsfn81ybp41q/mhealth-supply-chain-tanzania.pdf?dl=1",
     hubspot_form="95287374-5948-49e2-b67f-64e25723c355",
