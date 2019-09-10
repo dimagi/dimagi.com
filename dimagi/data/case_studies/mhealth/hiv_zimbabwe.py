00267a56e8be9932cfebb10ddb139786726365d2
hemanth <hemanth8877@gmail.com>
2019-09-10 11:57:18 +0530

    Added content (Image,PDF) pending

--- dimagi/data/case_studies/mhealth/hiv_zimbabwe.py
+++ dimagi/data/case_studies/mhealth/hiv_zimbabwe.py
@@ -5,33 +5,35 @@ from dimagi.pages.models.case_studies import CaseStudy
 
 STUDY = CaseStudy(
     title=ugettext_lazy(
-        "Using Mobile Rapid Diagnostic Tests for HIV and Malaria in Zimbabwe"
+        "Global Solutions for Infectious Diseases: Reporting Rapid "
+        "Diagnostic Test Results"
     ),
     summary=ugettext_lazy(
-        "With support from Econet Wireless and the Zimbabwean Ministry "
-        "of Health, Global Solutions for Infectious Diseases (GSID) "
-        "created a mobile system with Dimagi and ODK Diagnostics to "
-        "better process rapid diagnostic tests for HIV and malaria. "
-        "The system utilizes CommCare’s case management and reporting "
-        "features to digitize health workers’ workloads, and ODK-Dx’s "
-        "ability to process, analyze, and return RDT results with computer "
-        "vision algorithms."
+        "With support from Econet Wireless and the Zimbabwean Ministry of "
+        "Health, Global Solutions for Infectious Diseases (GSID) created a "
+        "mobile system with Dimagi and ODK Diagnostics to better process "
+        "rapid diagnostic tests for HIV and malaria. The system utilizes "
+        "CommCare’s case management and reporting features to digitize health "
+        "workers’ workloads, and ODK-Dx’s ability to process, analyze, and "
+        "return RDT results with computer vision algorithms."
     ),
     partners=[
-        "Zimbabwe Global Solutions for Infectious Diseases",
+        "Global Solutions for Infectious Diseases (GSID)",
+        "Zimbabwe Ministry of Health",
+        "Econet Wireless",
     ],
     countries=[
         ugettext_lazy("Zimbabwe"),
     ],
     sectors=[
-        ugettext_lazy("Malaria & HIV/AIDS"),
+        ugettext_lazy("Disease Treatment"),
     ],
     features=[
-        ugettext_lazy("Data collection"),
-        ugettext_lazy("automatic reporting"),
-        ugettext_lazy("RDT capturing & analysis"),
-        ugettext_lazy("health worker supervision"),
-        ugettext_lazy("process adherence"),
+        ugettext_lazy("Data Collection"),
+        ugettext_lazy("Automatic Reporting"),
+        ugettext_lazy("RDT Capturing & Analysis"),
+        ugettext_lazy("Health Worker Supervision"),
+        ugettext_lazy("Process Adherence"),
     ],
     slug="mhealth-hiv-zimbabwe",
     download_url="https://www.dropbox.com/s/2f8y8khz8amuios/mhealth-hiv-zimbabwe.pdf?dl=1",
