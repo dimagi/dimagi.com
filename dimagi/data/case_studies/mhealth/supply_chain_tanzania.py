8494f5fb2fb3fa6c0695245222e2fed45b93dfca
hemanth <hemanth8877@gmail.com>
2019-09-25 18:23:04 +0530

    Added PDF

--- dimagi/data/case_studies/mhealth/supply_chain_tanzania.py
+++ dimagi/data/case_studies/mhealth/supply_chain_tanzania.py
@@ -36,6 +36,6 @@ STUDY = CaseStudy(
         ugettext_lazy("Custom Reports"),
     ],
     slug="mhealth-supply-chain-tanzania",
-    download_url="https://www.dropbox.com/s/suffsfn81ybp41q/mhealth-supply-chain-tanzania.pdf?dl=1",
+    download_url="https://cdn2.hubspot.net/hubfs/503070/Case%20Studies/CommCare%20-%20ILSGateway%20Case%20Study.pdf",
     hubspot_form="95287374-5948-49e2-b67f-64e25723c355",
 )
