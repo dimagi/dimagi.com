78c71e703e96f152a6b650d05b53d4978bbaab5c
hemanth <hemanth8877@gmail.com>
2020-02-03 16:04:30 +0530

    Added chapter 4 and made changes to accordion for the chapter4 section

--- assets/js/app/accordion.js
+++ assets/js/app/accordion.js
@@ -21,6 +21,15 @@ define([
            $accordion.find('.accordion-content').slideUp();
         }
      });
+     $accordion.find('.accordion-custom-header').click(function () {
+      if ($accordion.hasClass('collapsed')) {
+         $accordion.removeClass('collapsed');
+         $accordion.find('.accordion-custom-content').slideDown();
+      } else {
+         $accordion.addClass('collapsed');
+         $accordion.find('.accordion-custom-content').slideUp();
+      }
+   });
     });
 
     _.each($(".accordion-group"), function (elem) {
