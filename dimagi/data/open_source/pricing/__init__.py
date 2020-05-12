a84562f828a0d80f16769accbe579ce40ce56479
hemanth <hemanth8877@gmail.com>
2020-05-12 18:39:40 +0530

    Seperated sections in accordion

--- dimagi/data/open_source/pricing/__init__.py
+++ dimagi/data/open_source/pricing/__init__.py
@@ -1,5 +1,6 @@
 from dimagi.data.open_source.pricing import (
     saas_platform,
+    local_datastorage,
     local_production,
     self_hosting
 
@@ -8,6 +9,7 @@ from dimagi.data.open_source.pricing import (
 
 feature_groups = (
     saas_platform,
+    local_datastorage,
     local_production,
     self_hosting
 )
--- dimagi/data/open_source/pricing/saas_platform.py
+++ dimagi/data/open_source/pricing/saas_platform.py
@@ -43,37 +43,4 @@ GROUP.add_summary([
             "CommCare developer forum"
         ),
     ),
-    Summary(
-        title=ugettext_lazy(
-            "Local Mirrored Data Storage"
-        ),
-        description=ugettext_lazy(
-            "The CommCare platform is supported by the Dimagi-hosted SaaS "
-            "environment. This service includes system administration tasks "
-            "such as regular CommCare code deploys, upgrading services, applying "
-            "data security measures, and maintaining platform up-time. Additionally, "
-            "your local team stands up and hosts an SQL database that stores the data "
-            "locally, and a data pipeline is established between the platform and the "
-            "local database, using manual intervention or CommCare features, such as "
-            "the Data Export Tool."
-        ),
-        specifics=ugettext_lazy(
-            "Data will be collected in the Dimagi-hosted environment "
-            "and copied to the local database."
-        ),
-        intensity=ugettext_lazy(
-            "Low"
-        ),
-        time=ugettext_lazy(
-            "Immediate"
-        ),
-        security=ugettext_lazy(
-            "Variable"
-        ),
-        support=ugettext_lazy(
-            "DevOps and infrastructure management (Dimagi-hosted environment only)"
-            "CommCare product support"
-            "CommCare developer forum"
-        ),
-    ),
 ])
