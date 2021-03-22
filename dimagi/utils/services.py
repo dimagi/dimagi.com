a5dacc04d93c3c40d1da6e133d46d2194ae65f91
Hemanth <hemanth8877gmail.com>
2021-03-23 02:55:24 +0530

    Update feedback changes

---
+++ dimagi/utils/services.py
@@ -0,0 +1,70 @@
+
+
+WEB = [
+    [
+        "svg/partner/upenn.html",
+        "svg/partner/upenn.html",
+        "svg/partner/upenn.html",
+    ],
+    [
+        "svg/partner/upenn.html",
+        "svg/partner/upenn.html",
+        "svg/partner/upenn.html",
+        "svg/partner/upenn.html",
+    ],
+    [
+        "svg/partner/upenn.html",
+        "svg/partner/upenn.html",
+    ],
+    [
+        "svg/partner/upenn.html",
+        "svg/partner/upenn.html",
+    ],
+    [
+        "svg/partner/upenn.html",
+        "svg/partner/upenn.html",
+    ],
+    [
+        "svg/partner/upenn.html",
+        "svg/partner/upenn.html",
+    ],
+]
+
+
+MOBILE = [
+    [
+        "svg/partner/rainforest_alliance.html",
+        "svg/partner/unicef.html",
+        "svg/partner/upenn.html",
+        "svg/partner/usaid.html",
+    ],
+    [
+        "svg/partner/bu.html",
+        "svg/partner/mercy_corps.html",
+        "svg/partner/pih.html",
+        "svg/partner/novartis.html",
+        "svg/partner/dsme.html",
+    ],
+    [
+        "svg/partner/abt.html",
+        "svg/partner/crs.html",
+        "svg/partner/irc.html",
+        "svg/partner/oikoi.html",
+        "svg/partner/sbir.html",
+    ],
+    [
+        "svg/partner/gates.html",
+        "svg/partner/jhu.html",
+        "svg/partner/tdh.html",
+        "svg/partner/bwh.html",
+        "svg/partner/one_acre_fund.html",
+        "svg/partner/nih.html",
+    ],
+]
+
+
+def get_service_partners():
+    return {
+        'mobile': MOBILE,
+        'web': WEB,
+    }
