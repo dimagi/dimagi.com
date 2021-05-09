cc347b7ad8f20e3a17a6c8e0c6eaf9583d056f78
Hemanth <hemanth8877gmail.com>
2021-05-10 03:53:45 +0530

    enterprise copy and assets update

--- dimagi/utils/enterprise_partners.py
+++ dimagi/utils/enterprise_partners.py
@@ -3,27 +3,25 @@
 WEB = [
     [
         "svg/partner/enterprise/af.html",
-        "svg/partner/enterprise/cf.html",
     ],
     [
         "svg/partner/enterprise/crs.html",
         "svg/partner/enterprise/goal.html",
     ],
     [
+        "svg/partner/enterprise/cf.html",
+    ],
+    [
+        
         "svg/partner/enterprise/irc.html",
         "svg/partner/enterprise/ss.html",
     ],
     [
         "svg/partner/enterprise/tf.html",
+    ],
+    [
         "svg/partner/enterprise/ts.html",
-    ],
-    [
         "svg/partner/enterprise/wv.html",
-        "svg/partner/enterprise/crs.html",
-    ],
-    [
-        "svg/partner/enterprise/goal.html",
-        "svg/partner/enterprise/ss.html",
     ],
 ]
 
