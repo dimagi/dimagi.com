8a460fc5d4b9023ac9befee9e93686a243cbb647
Hemanth <hemanth8877gmail.com>
2021-08-26 20:14:45 +0530

    New changes

--- dimagi/data/case_studies/mhealth/abt_associates_vectorlink.py
+++ dimagi/data/case_studies/mhealth/abt_associates_vectorlink.py
@@ -5,48 +5,57 @@ from dimagi.pages.models.case_studies import CaseStudy
 
 STUDY = CaseStudy(
     title=ugettext_lazy(
-        "MOTECH Suite for Continuum of Care Services in India"
+        "Abt Associates – VectorLink: CommCare for Reducing Malaria"
     ),
     summary=[
         ugettext_lazy(
-            "CARE India, Dimagi and Grameen Foundation deployed the MOTECH "
-            "Suite to improve delivery of family health interventions and "
-            "quality of health services in rural India. "
+            "The Malaria Initiative Africa Indoor Residual Spraying (PMI AIRS) "
+            "program is dedicated to reducing malaria in Africa through an "
+            "innovative, community-driven approach that includes Indoor Residual "
+            "Spraying (IRS). IRS involves spraying insecticide on the walls, "
+            "ceilings, and other common indoor resting places of mosquitoes."
         ),
         ugettext_lazy(
-            "The system, called the Continuum of Care Services (CCS), offers "
-            "a comprehensive tool for mobile workers, providing coordinated "
-            "care for one million people in Bihar, India. Its aim is to cover "
-            "the 1000-day window of pregnancy through the child’s second year "
-            "of age, to deliver ‘the right messages at the right time’ to "
-            "pregnant women and newborn children within the community. "
+            "In 2014, Dimagi joined the US President Malaria Initiative (PMI) and "
+            "Abt Associates to provide an mHealth solution for sustainable intervention. "
+            "The tools and systems provide efficient access to daily spray activity "
+            "indicators and improve supervisory efforts. The project has resulted in "
+            "preventing millions from contracting malaria throughout the African continent."
         ),
     ],
     partners=[
-        "Bill & Melinda Gates Foundation",
-        "Government of Bihar",
-        "CARE India",
-        "BBC Media Action",
-        "Grameen Foundation",
-        "Dimagi"
+        "Abt Associates",
+        "PMI VectorLink",
     ],
     countries=[
-        ugettext_lazy("India"),
+        ugettext_lazy("Benin"),
+        ugettext_lazy("Burkina Faso"),
+        ugettext_lazy("Côte d’Ivoire"),
+        ugettext_lazy("Ethiopia"),
+        ugettext_lazy("Ghana"),
+        ugettext_lazy("Kenya"),
+        ugettext_lazy("Madagascar"),
+        ugettext_lazy("Malawi"),
+        ugettext_lazy("Mali"),
+        ugettext_lazy("Mozambique"),
+        ugettext_lazy("Rwanda"),
+        ugettext_lazy("Senegal"),
+        ugettext_lazy("Sierra Leone"),
+        ugettext_lazy("Tanzania"),
+        ugettext_lazy("Uganda"),
+        ugettext_lazy("Zambia"),
+        ugettext_lazy("Zimbabwe"), 
     ],
     sectors=[
-        ugettext_lazy("Maternal, Newborn & Child Health including Nutrition (MNCHN)"),
+        ugettext_lazy("Infectious Disease"),
+        ugettext_lazy("Maternal & Child Health"),
     ],
     features=[
-        ugettext_lazy("Decision support"),
-        ugettext_lazy("Client management"),
-        ugettext_lazy("Data collection & sharing"),
-        ugettext_lazy("Mobile reports"),
-        ugettext_lazy("Home visit scheduler"),
-        ugettext_lazy("Call center"),
-        ugettext_lazy("Referral system"),
-        ugettext_lazy("EDD & BMI calculators"),
-        ugettext_lazy("Multimedia"),
-        ugettext_lazy("Performance dashboards"),
+        ugettext_lazy("Case Management"),
+        ugettext_lazy("Custom Reporting"),
+        ugettext_lazy("Two-way SMS Functionality"),
+        ugettext_lazy("Conditional Alerts"),
+        ugettext_lazy("Submission of Form via SMS"),
     ],
     slug="abt-associates-vectorlink",
     primary_cta="693139c3-51b7-4c58-a666-a049ee806d98",
