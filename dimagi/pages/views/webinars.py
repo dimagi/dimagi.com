183181dd250f232883fc69548c1d476c7931077c
Hemanth <hemanth8877gmail.com>
2021-10-07 02:56:30 +0530

    Changes from feedback

--- dimagi/pages/views/webinars.py
+++ dimagi/pages/views/webinars.py
@@ -19,3 +19,40 @@ def webinar_home(request):
 @enable_ab_test(DEMO_WORKFLOW_V2)
 def global_me(request):
     return render(request, 'pages/webinars/global_me.html')
+
+@enable_ab_test(DEMO_WORKFLOW_V2)
+def data_already(request):
+    return render(request, 'pages/webinars/data_already.html')
+
+@enable_ab_test(DEMO_WORKFLOW_V2)
+def optimize(request):
+    return render(request, 'pages/webinars/optimize.html')
+
+@enable_ab_test(DEMO_WORKFLOW_V2)
+def interoperability(request):
+    return render(request, 'pages/webinars/interoperability.html')
+
+@enable_ab_test(DEMO_WORKFLOW_V2)
+def adapt(request):
+    return render(request, 'pages/webinars/adapt.html')
+
+@enable_ab_test(DEMO_WORKFLOW_V2)
+def new_function(request):
+    return render(request, 'pages/webinars/new_function.html')
+
+@enable_ab_test(DEMO_WORKFLOW_V2)
+def data_clean(request):
+    return render(request, 'pages/webinars/data_clean.html')
+
+@enable_ab_test(DEMO_WORKFLOW_V2)
+def remote(request):
+    return render(request, 'pages/webinars/remote.html')
+
+@enable_ab_test(DEMO_WORKFLOW_V2)
+def covid19(request):
+    return render(request, 'pages/webinars/covid19.html')
+
+@enable_ab_test(DEMO_WORKFLOW_V2)
+def power_bi(request):
+    return render(request, 'pages/webinars/power_bi.html')
+
