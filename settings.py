7f8442170c9fa8838a2a0351b0bd96aed8ec2df5
Biyeun Buczyk <bbuczyk@dimagi.com>
2021-12-20 14:39:17 +0100

    try redis tls url

--- settings.py
+++ settings.py
@@ -112,7 +112,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 CACHES = {
-    'default': env.cache('REDIS_URL'),
+    'default': env.cache('REDIS_TLS_URL'),
 }
 
 # Static files (CSS, JavaScript, Images)
