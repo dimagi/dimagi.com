d4823d854193449cc58713228ff52c33d478a004
Biyeun Buczyk <bbuczyk@dimagi.com>
2021-12-20 22:31:19 +0100

    disable ab test on home

--- dimagi/pages/views/core.py
+++ dimagi/pages/views/core.py
@@ -20,7 +20,6 @@ from dimagi.utils.wordpress_api import get_json
 from dimagi.pages.models.partners import latestPartners
 
 
-@enable_ab_test(DEMO_WORKFLOW_V2)
 def home(request):
     context = {
         'partners': get_logos(),
