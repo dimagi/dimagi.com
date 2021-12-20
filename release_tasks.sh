10c3dba827062edb939af6a71a79a2e2d6a831e0
Biyeun Buczyk <bbuczyk@dimagi.com>
2021-12-20 18:01:34 +0100

    put back release tasks

--- release_tasks.sh
+++ release_tasks.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 python manage.py migrate
-#python manage.py build_thumbnails
-#python manage.py compilescss
-#python manage.py collectstatic --ignore=*.scss --clear --no-input
-#python manage.py compress
-#python manage.py build_static_versions
-#python manage.py compilescss --delete-files
-# python manage.py s3_pushstatic
+python manage.py build_thumbnails
+python manage.py compilescss
+python manage.py collectstatic --ignore=*.scss --clear --no-input
+python manage.py compress
+python manage.py build_static_versions
+python manage.py compilescss --delete-files
+python manage.py s3_pushstatic
