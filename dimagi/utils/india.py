fa64c3376fd69a06c7977a213b2f2297c48e4939
Hemanth <hemanth8877gmail.com>
2022-08-31 02:36:26 +0530

    Add feedback styles

--- dimagi/utils/india.py
+++ dimagi/utils/india.py
@@ -3,29 +3,22 @@
 WEB = [
     [
         "svg/partner/india/usaid.html",
-        "svg/partner/india/pathfinder.html",
     ],
     [
         "svg/partner/india/tdh.html",
-        "svg/partner/india/jnj.html",
-        "svg/partner/india/ipe.html",
     ],
     [
         "svg/partner/india/crs.html",
-        "svg/partner/india/tdh.html",
     ],
     [
         "svg/partner/india/gates.html",
-        "svg/partner/india/usaid.html",
     ],
     [
         "svg/partner/india/ipe.html",
-        "svg/partner/india/jnj.html",
-        "svg/partner/india/crs.html",
     ],
     [
         "svg/partner/india/pathfinder.html",
-        "svg/partner/india/gates.html",
+        "svg/partner/india/jnj.html",
     ],
 ]
 
