aefe3eb2a983ef7816f3f12ae37f14685a44c38d
Aswini <aswini.rachagundla@fisslabs.com>
2022-10-19 19:23:41 +0530

    commcare in the classroom-LP

--- dimagi/pages/views/commcare.py
+++ dimagi/pages/views/commcare.py
@@ -42,6 +42,9 @@ def pricing(request):
 def research(request):
     return render(request, 'pages/commcare/research.html')
 
+def classroom(request):
+    return render(request, 'pages/commcare/classroom.html')
+
 
 def handle_pricing_pdf(is_monthly):
 
